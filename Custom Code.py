88633d924cc95158acc5d2767c59cbe74d6e8f19
Krunalkumar Shah <einstein1410@gmail.com>
2025-01-19 21:04:49 +0530

    enhancement added like get owner name using owner api, removed html tags from summary and added a few extra field.

--- Custom Code.py
+++ Custom Code.py
@@ -1,7 +1,8 @@
-import os, requests
+import os, requests, re
 
 def main(event):
   
+  token = os.getenv("RevOps")
   meeting_id = event.get("inputFields").get("meeting_id")
   meeting_date = event.get("inputFields").get("meeting_date")
   setter = event.get("inputFields").get("setter")
@@ -10,15 +11,33 @@ def main(event):
   summary = event.get("inputFields").get("summary")
   meeting_start_time = event.get("inputFields").get("meeting_start_time")
   meeting_end_time = event.get("inputFields").get("meeting_end_time")
+  title = event.get("inputFields").get("title")
+  assignee = event.get("inputFields").get("assignee")
+  mtype = event.get("inputFields").get("type")
+  summary = re.sub(r'<[^>]+>', '', summary)
   
-  token = os.getenv("RevOps")
-  custom_object_id = "2-39538272"
-  record_created = 0
-  url = "https://api.hubapi.com/crm/v3/objects"
   headers = {
     "Authorization": f"Bearer {token}",
     "Content-Type": "application/json"
   }
+  
+  if "BDR - " in summary:
+    temp = summary.split("BDR - ")
+    setter = temp[1]
+  else:
+    owners_url = f"https://api.hubapi.com/owners/v2/owners/{assignee}"
+    response = requests.get(owners_url, headers=headers)
+    if response.status_code == 200:
+      user = response.json()
+      firstName = user.get("firstName", "")
+      lastName = user.get("lastName", "")
+      setter = firstName + " " + lastName
+    else:
+      setter = "User Not Found"
+  
+  custom_object_id = "2-39538272"
+  record_created = 0
+  url = "https://api.hubapi.com/crm/v3/objects"
   custom_object_data = {
     "properties": {
       "meeting_record_id": meeting_id,
@@ -29,10 +48,13 @@ def main(event):
       "meeting_start_time": meeting_start_time,
       "meeting_end_time": meeting_end_time,
       "disqualified_reason": disqualified_reason,
+      "meeting_title": title,
+      "meeting_type": mtype
     }
   }
   
   response = requests.post(f"{url}/{custom_object_id}", headers=headers, json=custom_object_data)
+  print(response.json())
   
   if response.status_code == 201:
     record_created = 1
@@ -49,6 +71,8 @@ def main(event):
       "End_Time": meeting_end_time,
       "Disqualified_Reason": disqualified_reason,
       "Summary": summary,
+      "Meeting_Type": mtype,
+      "Meeting_Title": title,
       "Record_Created": record_created
     }
   }
